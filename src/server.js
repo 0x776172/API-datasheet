e521e196e42e4b45a208cebcc14372a622fb6feb
0x776172 <ipul.anwar313@gmail.com>
2023-08-31 13:26:25 +0700

    updae

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 require("dotenv").config()
-const config = require("./config.json")
+const config = require("../config.json")
 const express = require("express")
-const router = require("./app/routes/router.js")
+const router = require("../app/routes/router.js")
+const serverless = require("serverless-http")
 
 const app = express()
 app.use(express.json())
@@ -15,3 +16,6 @@ app.use("/api/datasheet", router)
 app.listen(config.PORT, () => {
   console.log(`Connected to port ${config.PORT}`)
 })
+
+module.exports = app
+module.exports.handler = serverless(app)
