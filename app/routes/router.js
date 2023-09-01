f820a568abc2fd8faa343d0e47ff4a0019e497c5
0x776172 <ipul.anwar313@gmail.com>
2023-09-02 04:02:22 +0700

    Update router.js

--- app/routes/router.js
+++ app/routes/router.js
@@ -4,7 +4,7 @@ const family = require("../controllers/family.controller.js")
 const product = require("../controllers/product.controller.js")
 const desc = require("../controllers/desc.controller.js")
 
-router.post('/hello', (req, res) => res.send("Hello, Choose your data"))
+router.post('/hello', (req, res) => res.json({ status: 200, message: "Welcome!" }))
 router.post('/manufacturers', manufacturer.create)
 router.get('/manufacturers', manufacturer.get)
 router.get('/manufacturers/:id', manufacturer.get)
